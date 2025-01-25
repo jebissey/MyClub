ddc5f08fe21789baa4a1cf39eaf24b6877a7a4cc
jebissey <je.bissey@gmail.com>
2025-01-25 17:51:43 +0100

    Removed files

--- doc/LinesByFiles.cmd
+++ doc/LinesByFiles.cmd
@@ -1,2 +1,2 @@
-find ../MyClub/ -path "*/lib/jpgraph" -prune -o -path "*/lib/PhpMailer" -prune -o -name "*.php" -type f -exec wc -l {} + > LinesByFiles.txt
+find ../ -path "*/vendor" -prune -o -name "*.php" -type f -exec wc -l {} + > LinesByFiles.txt
 
