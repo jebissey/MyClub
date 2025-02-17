04d3ffcf10d212e0c87fc29858c555b27141d14e
jebissey <je.bissey@gmail.com>
2025-02-17 16:03:00 +0100

    updated lines by files

--- dev/LinesByFiles.cmd
+++ dev/LinesByFiles.cmd
@@ -1,2 +1,9 @@
-find ../ -path "*/vendor" -prune -o -path "*/var" -prune -o  -name "*.php" -type f -exec wc -l {} + > LinesByFiles.txt
-
+(
+  echo "=== Détail par fichier ===";
+  find ../WebSite/ -path "*/vendor" -prune -o -path "*/var" -prune -o \( -name "*.php" -o -name "*.latte" \) -type f -exec wc -l {} +;
+  echo -e "\n=== Total par type de fichier ===";
+  echo "Fichiers PHP :";
+  find ../WebSite/ -path "*/vendor" -prune -o -path "*/var" -prune -o -name "*.php" -type f -exec wc -l {} + | awk 'END {print $1}';
+  echo "Fichiers Latte :";
+  find ../WebSite/ -path "*/vendor" -prune -o -path "*/var" -prune -o -name "*.latte" -type f -exec wc -l {} + | awk 'END {print $1}';
+) > LinesByFiles.txt
