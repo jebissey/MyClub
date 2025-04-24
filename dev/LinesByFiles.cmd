36b32da88ce9f4594e4a3488f930f700c3d24bf9
jebissey <je.bissey@gmail.com>
2025-04-24 19:44:46 +0200

    Udated LinesByFiles.cmd plus some fixes

--- dev/LinesByFiles.cmd
+++ dev/LinesByFiles.cmd
@@ -1,9 +1,11 @@
 (
   echo "=== Détail par fichier ===";
-  find ../WebSite/ -path "*/vendor" -prune -o -path "*/var" -prune -o \( -name "*.php" -o -name "*.latte" \) -type f -exec wc -l {} +;
+  find ../WebSite/ -path "*/vendor" -prune -o -path "*/var" -prune -o \( -name "*.php" -o -name "*.latte" -o -name "*.js" \) -type f -exec wc -l {} +;
   echo -e "\n=== Total par type de fichier ===";
   echo "Fichiers PHP :";
   find ../WebSite/ -path "*/vendor" -prune -o -path "*/var" -prune -o -name "*.php" -type f -exec wc -l {} + | awk 'END {print $1}';
   echo "Fichiers Latte :";
   find ../WebSite/ -path "*/vendor" -prune -o -path "*/var" -prune -o -name "*.latte" -type f -exec wc -l {} + | awk 'END {print $1}';
+  echo "Fichiers Javascript :";
+  find ../WebSite/ -path "*/vendor" -prune -o -path "*/var" -prune -o -name "*.js" -type f -exec wc -l {} + | awk 'END {print $1}';
 ) > LinesByFiles.txt
