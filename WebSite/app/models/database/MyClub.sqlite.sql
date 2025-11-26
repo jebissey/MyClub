7a12ce8de5e8e38eff1e10ff95d981c3373cbae7
JEB <je.bissey@gmail.com>
2025-11-26 10:48:58 +0100

    Update DB

--- WebSite/app/models/database/MyClub.sqlite.sql
+++ WebSite/app/models/database/MyClub.sqlite.sql
@@ -206,6 +206,11 @@ CREATE TABLE IF NOT EXISTS "Metadata" (
 	"SendEmailAddress"	TEXT,
 	"SendEmailPassword"	TEXT,
 	"SendMailHost"	TEXT,
+	"Compact_maxRecords"	INTEGER NOT NULL DEFAULT 1000000,
+	"Compact_lastDate"	TEXT,
+	"Compact_everyXdays"	INTEGER NOT NULL DEFAULT 10,
+	"Compact_removeOlderThanXmonths"	INTEGER NOT NULL DEFAULT 36,
+	"Compact_compactOlderThanXmonths"	INTEGER NOT NULL DEFAULT 6,
 	PRIMARY KEY("Id")
 );
 CREATE TABLE IF NOT EXISTS "Need" (
@@ -772,7 +777,7 @@ INSERT INTO "Languages" VALUES (75,'ErrorLyricsFileReadError','''<div class=''''
                         <a href=''/'' class=''''btn btn-primary mt-4''''>🏠 Retour à l’accueil</a>
                     </div>
                 </div>');
-INSERT INTO "Metadata" VALUES (1,'MyClub',3,0,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO "Metadata" VALUES (1,'MyClub',4,0,NULL,NULL,NULL,NULL,NULL,1000000,NULL,10,36,6);
 INSERT INTO "Person" VALUES (1,'webmaster@myclub.foo','e427c26faca947919b18b797bc143a35100e4de48c34b70b26202d3a7d8e51f7','my first name','my last name','my nick name or nothing',NULL,'0',NULL,NULL,NULL,NULL,0,0,NULL,NULL,'2025-01-01',0,NULL,NULL,NULL,NULL);
 INSERT INTO "PersonGroup" VALUES (1,1,1);
 INSERT INTO "Settings" VALUES (1,'Title','title');
