37d0e313ee4ea18910c78fb727a8c517bf2fd92c
JEB <je.bissey@gmail.com>
2025-12-03 15:25:13 +0100

    Improved LinesByFiles

--- dev/LinesByFiles.cmd
+++ dev/LinesByFiles.cmd
@@ -1,6 +1,6 @@
 (
   echo "=== Détail par fichier ===";
-  find ../WebSite/ -path "*/vendor" -prune -o -path "*/var" -prune -o \( -name "*.php" -o -name "*.latte" -o -name "*.js" \) -type f -exec wc -l {} +;
+  find ../WebSite/ -path "*/vendor" -prune -o -path "*/var" -prune -o \( -name "*.php" -o -name "*.latte" -o -name "*.js" \) -type f -exec wc -l {} \; | grep -v total | sort -k2;
   echo -e "\n=== Total par type de fichier ===";
   echo "Fichiers PHP :";
   find ../WebSite/ -path "*/vendor" -prune -o -path "*/var" -prune -o -name "*.php" -type f -exec wc -l {} + | awk 'END {print $1}';
@@ -8,4 +8,6 @@
   find ../WebSite/ -path "*/vendor" -prune -o -path "*/var" -prune -o -name "*.latte" -type f -exec wc -l {} + | awk 'END {print $1}';
   echo "Fichiers Javascript :";
   find ../WebSite/ -path "*/vendor" -prune -o -path "*/var" -prune -o -name "*.js" -type f -exec wc -l {} + | awk 'END {print $1}';
+  echo -e "\n=== Total général ===";
+  find ../WebSite/ -path "*/vendor" -prune -o -path "*/var" -prune -o \( -name "*.php" -o -name "*.latte" -o -name "*.js" \) -type f -exec wc -l {} + | tail -1 | sed 's/total/Total tous fichiers/';
 ) > LinesByFiles.txt
