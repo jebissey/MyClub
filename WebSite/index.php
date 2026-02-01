c713677b32d3c4296f03280ae3bef2ae6e8f9142
JEB <je.bissey@gmail.com>
2026-02-01 11:05:41 +0100

    Remove Quill

--- WebSite/index.php
+++ WebSite/index.php
@@ -29,7 +29,7 @@ use app\modules\Webmaster\MaintenanceController;
 
 $logDir = __DIR__ . '/var/tracy/log';
 if (!is_dir($logDir)) mkdir($logDir, 0777, true);
-$local_access = false;// ($_SERVER['REMOTE_ADDR'] === '127.0.0.1' || $_SERVER['REMOTE_ADDR'] === '::1');
+$local_access = ($_SERVER['REMOTE_ADDR'] === '127.0.0.1' || $_SERVER['REMOTE_ADDR'] === '::1');
 if ($local_access)
     Debugger::enable(Debugger::Development, $logDir);
 else Debugger::enable(Debugger::Production, $logDir);
